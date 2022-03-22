6d3140a711bd66eaf8a3cc2907b0df6d68f4357a
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-03-22 22:16:52 +0530

    fuction restructured

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -5,6 +5,7 @@ import "./products.css";
 import { useFilter } from '../../context/context';
 import FilterContainer from './FilterContainer';
 import { products } from "../../backend/db/products"
+import { FilterFunction } from './updatedProducts';
 
 const Products = () => {
 	const { filterState, filterDispatch } = useFilter();
@@ -17,66 +18,7 @@ const Products = () => {
 	// 	}))();
 	// }, [])
 
-	const updatedProducts = () => {
-		let updatedProductList = products;
-
-		if (filterState.excludeNotAvailable) {
-			updatedProductList = updatedProductList.filter((item) => item.availableOrNot)
-			console.log(updatedProductList)
-		}
-
-		if (filterState.offerItems) {
-			updatedProductList = updatedProductList.filter((item) => item.offer)
-			console.log(updatedProductList)
-		}
-
-		if (filterState.byCategory) {
-			if (filterState.byCategory === 'All') {
-				updatedProductList = products
-			} else {
-				updatedProductList = updatedProductList.filter((item) => item.categoryName === filterState.byCategory)
-				console.log(updatedProductList)
-			}
-		}
-
-		if (filterState.byPrice) {
-			if (filterState.byPrice === 'All') {
-				updatedProductList = products
-			} else if (filterState.byPrice === "Above 50 Below 100") {
-				updatedProductList = updatedProductList.filter((item) => item.priceNew >= 50 && item.priceNew < 100)
-			} else if (filterState.byPrice === "Above 100 Below 200") {
-				updatedProductList = updatedProductList.filter((item) => item.priceNew >= 100 && item.priceNew < 200)
-			} else if (filterState.byPrice === "Above 200 Below 300") {
-				updatedProductList = updatedProductList.filter((item) => item.priceNew >= 200 && item.priceNew < 300)
-			}
-		}
-
-		if (filterState.sortByRange) {
-			updatedProductList = filterState.sortByRange === 0
-				? updatedProductList
-				: [...updatedProductList].filter((product) => product.priceNew <= filterState.sortByRange);
-			console.log(updatedProductList)
-		}
-
-		if (filterState.isSort) {
-			updatedProductList = updatedProductList.sort((a, b) =>
-				filterState.isSort === 'lowToHigh' ? a.priceNew - b.priceNew : b.priceNew - a.priceNew
-			)
-			// console.log(updatedProductList)
-		}
-		if (filterState.byRating) {
-			if (filterState.byRating === '4StarAndAbove') {
-				updatedProductList = updatedProductList.filter((item) => item.rating >= 4)
-			} else if (filterState.byRating === '3StarAndAbove') {
-				updatedProductList = updatedProductList.filter((item) => item.rating >= 3)
-			} else if (filterState.byRating === '2StarAndAbove') {
-				updatedProductList = updatedProductList.filter((item) => item.rating >= 2)
-			} else {
-				updatedProductList = updatedProductList.filter((item) => item.rating >= 1)
-			}
-		}
-		return updatedProductList;
-	}
+	const updatedProducts = FilterFunction(products);
 
 	return (
 		< div >
@@ -87,7 +29,7 @@ const Products = () => {
 						{/* {updatedProducts().length} */}
 						<div className="box-container">
 							{
-								updatedProducts().map((item) => {
+								updatedProducts.map((item) => {
 									return (
 										<div key={item._id}>
 											<ProductCard item={item} />
