e2ffc32358bc9f51f291968aa0ce55497acface2
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-03-27 10:44:37 +0530

    bugs fixed

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -42,6 +42,7 @@ const AuthProvider = ({ children }) => {
 	const [authState, authDispatch] = useReducer(authReducer, initialState);
 
 	const signup = async (userDetails) => {
+		console.log('hello')
 		try {
 			const response = await axios.post('/api/auth/signup', {
 				firstName: userDetails.firstName,
@@ -49,10 +50,11 @@ const AuthProvider = ({ children }) => {
 				email: userDetails.email,
 				password: userDetails.password
 			})
-			localStorage.setItem('token', response.data.encodedToken);
-
+			console.log(response)
+			localStorage.setItem('token', response.data.encodedToken)
 			authDispatch({ type: 'SUCCESS_TOAST', payload: { name: response.data.createdUser.firstName, toastMessage: 'Signed Up', id: response.data.createdUser._id } })
 		} catch (error) {
+			console.log(error)
 		}
 	}
 
@@ -66,37 +68,40 @@ const AuthProvider = ({ children }) => {
 			localStorage.setItem("token", response.data.encodedToken)
 			authDispatch({ type: "SUCCESS_TOAST", payload: { name: response.data.foundUser.firstName, toastMessage: "Logged in", id: response.data.foundUser._id } })
 		} catch (error) {
+			console.log(error.response);
 		}
 	}
 
 	const testLogin = async () => {
+		console.log('hello')
 		try {
 			const response = await axios.post('/api/auth/login', {
 				email: 'test123@gmail.com',
 				password: 'testLogin@123'
 			})
 			localStorage.setItem('token', "fakelogintoken")
-
+			console.log(response)
 			authDispatch({ type: 'TEST_TOAST', payload: { name: response.data.foundUser.firstName, toastMessage: 'Logid In', id: response.data.foundUser._id } })
 		} catch (error) {
-
+			console.log(error)
 		}
-
-
-		const logout = () => {
-			localStorage.clear();
-			authDispatch({ type: 'LOGOUT', payload: { toastMessage: 'Logged Out', id: '' } })
-		}
-
-		return (
-			<authContext.Provider value={{ authState, authDispatch, signup, login, testLogin, logout }}>
-				{children}
-			</authContext.Provider>
-		)
 	}
 
 
-	const useAuth = () => useContext(authContext);
+	const logout = () => {
+		localStorage.clear();
+		authDispatch({ type: 'LOGOUT', payload: { toastMessage: 'Logged Out', id: '' } })
+	}
+
+	return (
+		<authContext.Provider value={{ authState, authDispatch, signup, login, testLogin, logout }}>
+			{children}
+		</authContext.Provider>
+	)
+}
 
 
-	export { useAuth, AuthProvider };
+const useAuth = () => useContext(authContext);
+
+
+export { useAuth, AuthProvider };
