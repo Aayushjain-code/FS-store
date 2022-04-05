30f6990d2bbf3cc251d17848db371cef3309fe43
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-04-05 09:05:44 +0530

    footer fixed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,74 +1,87 @@
-import { useState, useEffect } from 'react'
-import "./Navbar.css"
-import { Link } from 'react-router-dom'
-import { useAuth } from '../../context/authContext'
+import { useState, useEffect } from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-	const { logout, authState: { userName, loggedIn, cart }
-	} = useAuth()
-	const [cartQuantity, setCartQuantity] = useState(cart.length);
+  const {
+    logout,
+    authState: { userName, loggedIn, cart },
+  } = useAuth();
+  const [cartQuantity, setCartQuantity] = useState(cart.length);
 
-	useEffect(() => {
-		setCartQuantity(cart.length)
-	}, [cart])
+  useEffect(() => {
+    setCartQuantity(cart.length);
+  }, [cart]);
 
+  let navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/login");
+  };
 
-	let navigate = useNavigate();
-	const handleClick = () => {
-		navigate("/login")
-	}
+  const [msg1, setMsg1] = useState(false);
+  const [msg2, setMsg2] = useState(false);
 
-	const [msg1, setMsg1] = useState(false);
-	const [msg2, setMsg2] = useState(false);
+  return (
+    <div>
+      <header className="header">
+        <Link to="/" className="logo">
+          {" "}
+          <i className="fab fa-canadian-maple-leaf"></i> FreshStore{" "}
+        </Link>
 
-	return (
-		<div>
-			<header className="header">
+        <nav className="navbar">
+          <Link to="/">home</Link>
+          <Link to="/products">products</Link>
+          <Link to="/wishlist">Wishlist</Link>
+          <Link to="/cart">Cart</Link>
+        </nav>
+        <div className="icons">
+          <span
+            style={{
+              fontWeight: "bold",
+              margin: "1rem",
+              fontSize: "initial",
+            }}
+          >
+            Hi, {userName}
+          </span>
+          <div className="fas fa-search" id="search-btn"></div>
+          <div className="fas fa-moon" id="theme-btn"></div>
 
-				<Link to="/" className="logo"> <i className="fab fa-canadian-maple-leaf"></i> FreshStore </Link>
+          {loggedIn ? (
+            <>
+              <div
+                className="fas fa-sign-out-alt"
+                id="theme-btn"
+                onMouseEnter={() => setMsg2(true)}
+                onMouseLeave={() => setMsg2(false)}
+                onClick={() => logout()}
+              ></div>
+              {msg2 ? <span className="msgStyle1">Logout</span> : ""}
+            </>
+          ) : (
+            <>
+              <div
+                className="fas fa-user"
+                id="theme-btn"
+                onMouseEnter={() => setMsg1(true)}
+                onMouseLeave={() => setMsg1(false)}
+                onClick={handleClick}
+              ></div>
+              {msg1 ? <span className="msgStyle1">Login</span> : ""}
+            </>
+          )}
 
-				<nav className="navbar">
-					<Link to='/'><a>home</a></Link>
-					<Link to='/products'><a>products</a></Link>
-					<Link to='/wishlist'><a>Wishlist</a></Link>
-					<Link to='/cart'><a>Cart</a></Link>
-				</nav>
-				<div className="icons">
-					<span style={{
-						fontWeight: "bold",
-						margin: "1rem",
-						fontSize: "initial"
-					}}>Hi, {userName}</span>
-					<div className="fas fa-search" id="search-btn" ></div>
-					<div className="fas fa-moon" id="theme-btn" >
-					</div>
+          <div className="m1 badge-container">
+            <div className="fas fa-shopping-cart"></div>
+            <span className="badge right-badge sm-badge"> {cartQuantity} </span>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
 
-					{
-						loggedIn ?
-							<>
-								<div className="fas fa-sign-out-alt" id="theme-btn" onMouseEnter={() => setMsg2(true)}
-									onMouseLeave={() => setMsg2(false)} onClick={() => logout()}></div>
-								{msg2 ? <span className="msgStyle1">Logout</span> : ""}
-							</> :
-							<>
-								<div className="fas fa-user" id="theme-btn" onMouseEnter={() => setMsg1(true)}
-									onMouseLeave={() => setMsg1(false)} onClick={handleClick} ></div>
-								{msg1 ? <span className="msgStyle1">Login</span> : ""}
-							</>
-
-					}
-
-					<div className="m1 badge-container">
-						<div className="fas fa-shopping-cart"></div>
-						<span className="badge right-badge sm-badge"> {cartQuantity} </span>
-					</div>
-				</div>
-
-			</header>
-
-		</div>
-	)
-}
-
-export default Navbar
+export default Navbar;
