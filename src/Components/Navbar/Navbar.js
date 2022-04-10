e70a7915af1a24da1d60a06327df6028c1f68750
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-04-10 17:57:26 +0530

    bugs fixed

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = () => {
       <header className="header">
         <Link to="/" className="logo">
           {" "}
-          <i className="fab fa-canadian-maple-leaf"></i> FreshStore{" "}
+          <i className="fab fa-canadian-maple-leaf"></i> FreshStore
         </Link>
         <form action="" class="search-form">
           <input
@@ -66,47 +66,14 @@ const Navbar = () => {
         </nav>
 
         <div className="icons">
-          <span
-            style={{
-              fontWeight: "bold",
-              margin: "1rem",
-              fontSize: "initial",
-            }}
-          >
-            Hi, {userName}
-          </span>
+          <span className="user-text">Hi, {userName}</span>{" "}
           <div className="fas fa-search" id="search-btn"></div>
           <div
             onClick={toggleLightDarkTheme}
             className={`${theme === "light" ? "fas fa-moon" : "fas fa-sun"} `}
             id="theme-btn"
           ></div>
-
-          {/* {loggedIn ? (
-            <>
-              <div
-                className="fas fa-sign-out-alt"
-                id="theme-btn"
-                onMouseEnter={() => setMsg2(true)}
-                onMouseLeave={() => setMsg2(false)}
-                onClick={() => logout()}
-              ></div>
-              {msg2 ? <span className="msgStyle1">Logout</span> : ""}
-            </>
-          ) : (
-            <>
-              <div
-                className="fas fa-user"
-                id="theme-btn"
-                onMouseEnter={() => setMsg1(true)}
-                onMouseLeave={() => setMsg1(false)}
-                onClick={handleClick}
-              ></div>
-              {msg1 ? <span className="msgStyle1">Login</span> : ""}
-            </>
-          )} */}
-
-          <div className="m1 badge-container">
+          <div className="m1 badge-container" onClick={() => navigate("/cart")}>
             <div className="fas fa-shopping-cart"></div>
             <span className="badge right-badge sm-badge"> {cartQuantity} </span>
           </div>
